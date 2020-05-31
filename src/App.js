c1dd55888170cd7683da2d766e9f43f5c65023e6
waqasusmani <waqas-usmani@hotmail.com>
2020-06-01 02:04:04 +0500

    build commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>I added this heading to react's original code</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
